f4d1d5af0bd87349dd688e1604fd00fc3eea1d0a
B1QUAD <accntmail.745@gmail.com>
2021-08-04 15:49:58 -0500

    Fixed end of file

--- public/js/githubInterface.js
+++ public/js/githubInterface.js
@@ -7,60 +7,61 @@ var languages = {};
 // This needs to be run and completed before anything else!!!!
 async function githubApiInit() {
     await loadFromApi('/util/langs.json', {
-			method: 'GET',
-			headers : {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json'
- 			}
-		}).then(response => response['jsonResponse']).then(function(langs) {
-			languages = langs;
-		});
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }).then(response => response['jsonResponse']).then(function (langs) {
+        languages = langs;
+    });
 }
 
 
 // Rate limited to 10 requests per minute
 function getQuestion() {
-    return new Promise((resolve, reject) => {
+    return new Promise(async function (resolve, reject) {
         let currentLang = getRandomLang();
-    	let choices; // Array of js objects containing matching files
-    	let json;
-    	let returnObj = {
-    		codeRef: '', // url to github blob
-    		answer: [currentLang].concat(languages[currentLang])
-    	};
+        let choices; // Array of js objects containing matching files
+        let json;
+        let returnObj = {
+            codeRef: '', // url to github blob
+            answer: [currentLang].concat(languages[currentLang])
+        };
 
-	// Search for files with that language in them
-	// https://api.github.com/search/code?q=language:${lang}+repo:leachim6/Hello-World
-	let response = await loadFromApi(`https://api.github.com/search/code?q=language:${currentLang}+repo:leachim6/Hello-World`, {
-		method: 'GET',
-		headers: {
-			'Accept': 'application/vnd.github.v3+json'
-		}
-	});
+        // Search for files with that language in them
+        // https://api.github.com/search/code?q=language:${lang}+repo:leachim6/Hello-World
+        let response = await loadFromApi(`https://api.github.com/search/code?q=language:${currentLang}+repo:leachim6/Hello-World`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/vnd.github.v3+json'
+            }
+        });
 
-	json = await response['jsonResponse'];
+        json = await response['jsonResponse'];
 
-	choices = json['items'];
-	returnObj.codeRef = getRandomValFromArray(choices)['html_url'];
-	console.log('Return object', returnObj);
-	return returnObj;
+        choices = json['items'];
+        returnObj.codeRef = getRandomValFromArray(choices)['html_url'];
+        console.log('Return object', returnObj);
+        return returnObj;
+    });
 }
 
 // Gets and returns an array with numQuestions indices.
 // Capped at 10 questions per call
 async function getQuestions(numQuestions) {
-	let questions = [];
-	if (numQuestions > 10) {
-		console.log(`${numQuestions} would get rate limited after the 10th request.\n`);
-		numQuestions = 10; // Cap at 10 requests
-	}
+    let questions = [];
+    if (numQuestions > 10) {
+        console.log(`${numQuestions} would get rate limited after the 10th request.\n`);
+        numQuestions = 10; // Cap at 10 requests
+    }
 
-	for (let i = 0; i < numQuestions; i++) {
-		questions.push(await getQuestion());
-	}
+    for (let i = 0; i < numQuestions; i++) {
+        questions.push(await getQuestion());
+    }
 
-	console.log('Do not call getQuestion or getQuestions for another minute to avoid rate limiting.');
-	return questions;
+    console.log('Do not call getQuestion or getQuestions for another minute to avoid rate limiting.');
+    return questions;
 }
 
 // This loads data from a url given the url and a params obj to pass into fetch
@@ -78,28 +79,23 @@ async function getQuestions(numQuestions) {
 //     });
 // =======
 async function loadFromApi(url, params) {
-	return await fetch(url, params)
-        .then(async function(response) {
-			let status = response.status;
-			console.log(typeof status, '\n' + status);
-			let jsonResponse = await response.json();
-			console.log(jsonResponse);
+    return await fetch(url, params).then(async function (response) {
+        let status = response.status;
+        console.log(typeof status, '\n' + status);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
 
-			if(status === 403) {
-				alert('Rate limit for GitHub exceeded.\nPlease wait ~30 seconds before continuing play.')
-			} else if (status !== 200) {
-				alert('Error calling the GitHub API.\nPlease refresh the page.');
-			} else {
-				// window.location.href = window.location.search;
-			}
+        if (status === 403) {
+            alert('Rate limit for GitHub exceeded.\nPlease wait ~30 seconds before continuing play.')
+        } else if (status !== 200) {
+            alert('Error calling the GitHub API.\nPlease refresh the page.');
+        } else { // window.location.href = window.location.search;
+        }
 
-            return {
-                rawResponse: response,
-                jsonResponse: jsonResponse
-            };
-        }).catch(function(error) {
-			console.log(error);
-		});
+        return {rawResponse: response, jsonResponse: jsonResponse};
+    }).catch(function (error) {
+        console.log(error);
+    });
 }
 
 // ********************************
@@ -108,23 +104,21 @@ async function loadFromApi(url, params) {
 
 // Returns random language for use with generating questions
 function getRandomLang() {
-	let keys = Object.keys(languages);
-	// Returns a language given a random key
-	return keys[randFromRange(0, keys.length - 1)];
+    let keys = Object.keys(languages);
+    // Returns a language given a random key
+    return keys[randFromRange(0, keys.length - 1)];
 }
 
 // Does what it says on the can
 // Returns a value from the provided array from a pseudo-random index
-function getRandomValFromArray(array) {
-	// return array[Math.floor(Math.random() * array.length)];
-	return array[randFromRange(0, array.length - 1)];
+function getRandomValFromArray(array) { // return array[Math.floor(Math.random() * array.length)];
+    return array[randFromRange(0, array.length - 1)];
 }
 
 // Returns an integer between min and max inclusive
-function randFromRange (min, max) {
-	// Just to be safe
-	min = Math.ceil(min);
-	max = Math.floor(max);
+function randFromRange(min, max) { // Just to be safe
+    min = Math.ceil(min);
+    max = Math.floor(max);
 
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
