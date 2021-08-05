85965f2e60b4f7425e29cd2595b23e24c8293fb4
Matthew Ghera <mghera02@hotmail.com>
2021-08-05 23:00:16 +0000

    can sign out on dashboard and sign back in

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -3,15 +3,37 @@ let user;
 
 
 
-// We are using google auth for now bc it does not require github auth tokens
-function signIn() {
-	// Redirect to sign in page with options for google or github sign in
-}
+const checkIfSignedIn = () => {
+    /*firebase.auth().onAuthStateChanged(function(user) {
+        console.log(user);
+        if (user) {
+            console.log("sign out");
+            firebase.auth().signOut();
+            return;
+        }
+        else if (!user) {
+            console.log("sign in");
+            googleSignIn();
+            return;
+        }
+    });*/
+    console.log(userId);
+    if(userId.length>6){
+        console.log("sign out");
+        firebase.auth().signOut();
+        location.reload();
+    }else{
+        console.log("sign in");
+        googleSignIn();
+    }
+};
+
 
 // Handles sign in for google accounts. Can be used as an onClick handler
 // Docs: https://firebase.google.com/docs/reference/js/firebase.auth.GoogleAuthProvider
 const googleSignIn = () => {
-	console.log('signIn function called!');
+    console.log('signIn function called!');
+    
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
 	// Creates a new authentication provider for use with google accnts.
 	const provider = new firebase.auth.GoogleAuthProvider();
@@ -29,5 +51,16 @@ const googleSignIn = () => {
 		}).catch(error => {
 			// Handle the error
 			console.log(error);
-		});
+        });
+        
 };
+
+function googleSignout() {
+   firebase.auth().signOut()
+	
+   .then(function() {
+      console.log('Signout Succesfull')
+   }, function(error) {
+      console.log('Signout Failed')  
+   });
+}
