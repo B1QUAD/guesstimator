4d5ffab20b68dbe6efa70dabe08dfc3aa1de2740
Matthew Ghera <mghera02@hotmail.com>
2021-08-05 03:54:24 +0000

    added some navbar updates and getter function for user id

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,6 +1,15 @@
+let userId;
+let user;
+
+
+
 // We are using google auth for now bc it does not require github auth tokens
 function signIn() {
 	// Redirect to sign in page with options for google or github sign in
+}
+
+const getUserId = () => {
+    return userId;
 }
 
 // Handles sign in for google accounts. Can be used as an onClick handler
@@ -16,12 +25,86 @@ const googleSignIn = () => {
 		.then(result => {
 			const credential = result.credential;
 			const token = credential.accessToken;
-			const user = result.user;
+			user = result.user;
 
-			console.log(user.uid);
+            console.log(user.uid);
+            userId=user.uid;
 			window.location = 'dashboard.html';
 		}).catch(error => {
 			// Handle the error
 			console.log(error);
 		});
 };
+
+
+
+const categoryDropDown=document.querySelector("#category-select");
+const personalHeader=document.querySelector("#personal-header");
+const globalHeader=document.querySelector("#global-header");
+const profileName=document.querySelector("#profile-name");
+const signInBtn=document.querySelector("#signIn-btn");
+
+
+let category="Programming";
+
+window.onload = () =>{
+  changeDom();  
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        signInBtn.innerHTML="Change Accounts";
+    }
+    if (!user) {
+        signInBtn.innerHTML="Sign In"
+      userId="guest";
+    }
+  });
+};
+
+
+categoryDropDown.addEventListener('change', (e) => {
+    category=document.querySelector("#category-select").value;
+    changeDom();
+});
+
+const changeDom = () =>{
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            profileName.innerHTML=user.displayName;
+            user.providerData.forEach(profile => {
+                console.log(profile.photoURL);
+                document.querySelector("#profile-pic").src=profile.photoURL;
+            }); //this will give you all the urls once there is user data
+        }
+    });
+
+
+    if(category=="Programming"){
+        personalHeader.innerHTML="Top Programming Languages";
+        globalHeader.innerHTML="Global Programming Info";
+    }
+    else if(category=="Lyrics"){
+        personalHeader.innerHTML="Top Songs";
+        globalHeader.innerHTML="Global Lyric Info";
+    }
+    getUserStats();
+};
+
+const getUserStats = () => {
+	const userRef = firebase.database().ref(`users/${userId}`);
+	userRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+		//changeUserDom(data);
+	});
+} 
+
+const changeUserDom = (data) => {
+    if(category=="Programming"){
+        category="proglang";
+    }
+
+	if(data.games.gameMode==category){
+        console.log(data);
+    }
+}
