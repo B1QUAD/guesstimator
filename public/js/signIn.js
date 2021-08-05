ae3eec1c5c7b97d02c06451c49d657c47e8b81c1
Matthew Ghera <mghera02@hotmail.com>
2021-08-05 02:20:34 +0000

    dashboard update

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,3 +1,4 @@
+let userId, user;
 // We are using google auth for now bc it does not require github auth tokens
 function signIn() {
 	// Redirect to sign in page with options for google or github sign in
@@ -16,12 +17,75 @@ const googleSignIn = () => {
 		.then(result => {
 			const credential = result.credential;
 			const token = credential.accessToken;
-			const user = result.user;
+			user = result.user;
 
-			console.log(user.uid);
+            console.log(user.uid);
+            userId=user.uid;
 			window.location = 'dashboard.html';
 		}).catch(error => {
 			// Handle the error
 			console.log(error);
 		});
 };
+
+
+
+const categoryDropDown=document.querySelector("#category-select");
+const personalHeader=document.querySelector("#personal-header");
+const globalHeader=document.querySelector("#global-header");
+const profileName=document.querySelector("#profile-name");
+
+
+let category="Programming";
+
+window.onload = () =>{
+  changeDom();  
+};
+
+
+categoryDropDown.addEventListener('change', (e) => {
+    category=document.querySelector("#category-select").value;
+    changeDom();
+});
+
+const changeDom = () =>{
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            profileName.innerHTML=user.displayName;
+            user.providerData.forEach(profile => {
+                console.log(profile.photoURL);
+                document.querySelector("#profile-pic").src=profile.photoURL;
+            }); //this will give you all the urls once there is user data
+        }
+    });
+
+
+    if(category=="Programming"){
+        personalHeader.innerHTML="Top Programming Languages";
+        globalHeader.innerHTML="Global Programming Info";
+    }
+    else if(category=="Lyrics"){
+        personalHeader.innerHTML="Top Songs";
+        globalHeader.innerHTML="Global Lyric Info";
+    }
+    getUserStats();
+};
+
+const getUserStats = () => {
+	const userRef = firebase.database().ref(`users/${userId}`);
+	userRef.on('value', (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+		//changeUserDom(data);
+	});
+} 
+
+const changeUserDom = (data) => {
+    if(category=="Programming"){
+        category="proglang";
+    }
+
+	if(data.games.gameMode==category){
+        console.log(data);
+    }
+}
