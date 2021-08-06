69f9ee1c8c1d77a23b6d63d42b96105c7ce3762f
B1QUAD <accntmail.745@gmail.com>
2021-08-06 00:45:50 -0500

    Chart theming

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,110 +2,193 @@ const maxPersonalListEntries = 5;
 const maxGlobalListEntries = 10;
 const categoryDropDown = document.querySelector("#category-select");
 let category = categoryDropDown.value;
-const personalHeader=document.querySelector("#personal-header");
-const globalHeader=document.querySelector("#global-header");
-const profileName=document.querySelector("#profile-name");
+const personalHeader = document.querySelector("#personal-header");
+const globalHeader = document.querySelector("#global-header");
+const profileName = document.querySelector("#profile-name");
 category = "Programming";
 
-
-// if(category === "Programming"){
-//     category = "proglang";
-// }
-//
-// categoryDropDown.addEventListener("click", function(){
-//   getUserStats();
-// });
-//
-// const getUserStats = () => {
-// 	const userRef = firebase.database().ref(`users/${userId}`);
-// 	userRef.on('value', (snapshot) => {
-// 		const data = snapshot.val();
-// 		changeUserDom(data);
-// 	});
-// }
-//
-// const changeUserDom = (data) => {
-// 	if(data.games.gameMode === category){
-//         console.log(data);
-//     }
-// }
-
-// Sorts an array of objects
-
-const changeDom = () =>{
+const changeDom = () => {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-            userId=user.uid;
-            profileName.innerHTML=user.displayName;
+            userId = user.uid;
+            profileName.innerHTML = user.displayName;
             user.providerData.forEach(profile => {
                 console.log(profile.photoURL);
-                document.querySelector("#profile-pic").src=profile.photoURL;
+                document.querySelector("#profile-pic").src = profile.photoURL;
             }); //this will give you all the urls once there is user data
-        }else{
-            userId="guest";
+        } else {
+            userId = "guest";
         }
     });
 
 
-    if(category=="Programming"){
-        personalHeader.innerHTML="Top Programming Languages";
-        globalHeader.innerHTML="Global Programming Info";
-    }
-    else if(category=="Lyrics"){
-        personalHeader.innerHTML="Top Songs";
-        globalHeader.innerHTML="Global Lyric Info";
+    if (category == "Programming") {
+        personalHeader.innerHTML = "Top Programming Languages";
+    } else if (category == "Lyrics") {
+        personalHeader.innerHTML = "Top Songs";
     }
     getUserStats();
 };
 
 categoryDropDown.addEventListener('change', (e) => {
-    category=document.querySelector("#category-select").value;
+    category = document.querySelector("#category-select").value;
     changeDom();
 });
 
 const getUserStats = () => {
-	const userRef = firebase.database().ref(`users/${userId}`);
-	userRef.on('value', (snapshot) => {
+    const userRef = firebase.database().ref(`users/${userId}`);
+    userRef.on('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
-		//changeUserDom(data);
-	});
+        //changeUserDom(data);
+    });
 }
 
 const changeUserDom = (data) => {
-    if(category=="Programming"){
-        category="proglang";
+    if (category == "Programming") {
+        category = "proglang";
     }
 
-	if(data.games.gameMode==category){
+    if (data.games.gameMode == category) {
         console.log(data);
     }
 }
 
+function createChart() {
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        maintainAspectRatio: false,
+        responsive: false,
+		backgroundColor: 'rgba(0, 0, 0, 0.0)',
+		theme: 'dark1',
+
+        title:{
+        	text:"Your Stats"
+        },
+        axisX: {
+            interval: 1
+        },
+        axisY2: {
+            interlacedColor: "rgba(1,77,101,.2)",
+            gridColor: "rgba(1,77,101,.1)",
+            title: "-'s Score Over Time"
+        },
+        data: [{
+            type: "bar",
+            name: "scores",
+            axisYType: "primary",
+            color: "#50050a",
+            dataPoints: [{
+                    y: 3,
+                    label: "Sweden"
+                },
+                {
+                    y: 7,
+                    label: "Taiwan"
+                },
+                {
+                    y: 5,
+                    label: "Russia"
+                },
+                {
+                    y: 9,
+                    label: "Spain"
+                },
+                {
+                    y: 7,
+                    label: "Brazil"
+                },
+                {
+                    y: 7,
+                    label: "India"
+                },
+                {
+                    y: 9,
+                    label: "Italy"
+                },
+                {
+                    y: 8,
+                    label: "Australia"
+                },
+                {
+                    y: 11,
+                    label: "Canada"
+                },
+                {
+                    y: 15,
+                    label: "South Korea"
+                },
+                {
+                    y: 12,
+                    label: "Netherlands"
+                },
+                {
+                    y: 15,
+                    label: "Switzerland"
+                },
+                {
+                    y: 25,
+                    label: "Britain"
+                },
+                {
+                    y: 28,
+                    label: "Germany"
+                },
+                {
+                    y: 29,
+                    label: "France"
+                },
+                {
+                    y: 52,
+                    label: "Japan"
+                },
+                {
+                    y: 103,
+                    label: "China"
+                },
+                {
+                    y: 134,
+                    label: "US"
+                }
+            ]
+        }]
+    });
+    chart.render();
+
+}
+
 function sortObjects(obj, sortFunction) {
-	let list;
+    let list;
 
-
-	list.sort(w)
+    list.sort(w)
 }
 
 function dashInit() {
-	changeDom();
+    changeDom();
 
-	var db = firebase.database();
-	console.log('Hello there!!');
-	// Work on data vis and pulling from the db
-	// These are the ordered lists, append list item tags as children
-	const personalTopThings = document.querySelector('#personal-top');
-	const personalTopScores = document.querySelector('#personal-score');
+    var db = firebase.database();
+    console.log('Hello there!!');
+    // Work on data vis and pulling from the db
+    // These are the ordered lists, append list item tags as children
+    const personalTopThings = document.querySelector('#personal-top');
+    const personalTopScores = document.querySelector('#personal-score');
 
-	const globalHardest = document.querySelector('#hardest');
-	const globalRecog = document.querySelector('#easiest');
-	const globalLeader = document.querySelector('#leaderboard');
+    const globalHardest = document.querySelector('#hardest');
+    const globalRecog = document.querySelector('#easiest');
+    const globalLeader = document.querySelector('#leaderboard');
 
-	db.ref().once('value')
-  		.then(snapshot => snapshot.val()).then(function(data) {
-    	// Handle read data.
-		console.log('Test\n', data);
-  	});
+    db.ref().once('value')
+        .then(snapshot => snapshot.val()).then(function(data) {
+            // Handle read data.
+            console.log('Test\n', data);
+        });
+}
+
+function reloadCss() {
+    var links = document.getElementsByTagName("link");
+    for (var cl in links) {
+        var link = links[cl];
+        if (link.rel === "stylesheet")
+            link.href += "";
+    }
 }
