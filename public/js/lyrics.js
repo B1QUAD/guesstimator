35b5fd38670cab8eb7dd02d5eea0742183d6ffeb
Matthew Ghera <mghera02@hotmail.com>
2021-08-06 02:21:16 +0000

    dark mode modal

--- public/js/lyrics.js
+++ public/js/lyrics.js
@@ -16,7 +16,7 @@ https://developer.musixmatch.com/admin/applications
 
 let key="c1562ca1935a6e4ef23613a28c7fc6ca";
 let key2="a15480b2b104cdb7ec707f3876c7e392";
-key=key2;
+//key=key2;
 
 let numSongs=50;
 let topSongs=`https://radiant-ridge-05292.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=${numSongs}&country=US&f_has_lyrics=1&apikey=${key}`;
