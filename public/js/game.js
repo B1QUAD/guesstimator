1320df74d1d664b61225a30a4ad953decdefbc54
David Yates <djwyates@gmail.com>
2021-08-05 17:58:50 -0700

    refactored initializeGame function

--- public/js/game.js
+++ public/js/game.js
@@ -7,32 +7,30 @@ let checkAnswerTimeout;
 const questionBox = document.querySelector('#question-box');
 
 const initializeGame = () => {
-    return new Promise((resolve, reject) => {
-        getCurrentGame().then(currGameInfo => {
-            if (!currGameInfo.isReady) {
-                return resolve(false);
-            }
-            if (currentGame.gamemode === 'progLang') {
-                githubApiInit().then(result => {
-                    if (currGameInfo.needsNewQuestion) {
-                        getProgLangQuestion(currGameInfo.newQuestionTimestamp).then(result => {
-                            resolve(true);
-                        });
-                    } else {
-                        renderProgLangQuestion();
-                        resolve(true);
-                    }
-                });
-            } else if (currentGame.gamemode === 'lyrics') {
+    getCurrentGame().then(currGameInfo => {
+        if (!currGameInfo.isReady) {
+            return;
+        }
+        if (currentGame.gamemode === 'progLang') {
+            githubApiInit().then(result => {
                 if (currGameInfo.needsNewQuestion) {
-                    getLyricsQuestion(currGameInfo.newQuestionTimestamp);
-                    resolve(true);
+                    getProgLangQuestion(currGameInfo.newQuestionTimestamp).then(result => {
+                        refreshUI();
+                    });
                 } else {
-                    renderLyricsQuestion();
-                    resolve(true);
+                    renderProgLangQuestion();
+                    refreshUI();
                 }
+            });
+        } else if (currentGame.gamemode === 'lyrics') {
+            if (currGameInfo.needsNewQuestion) {
+                getLyricsQuestion(currGameInfo.newQuestionTimestamp);
+                refreshUI();
+            } else {
+                renderLyricsQuestion();
+                refreshUI();
             }
-        });
+        }
     });
 };
 
@@ -179,7 +177,7 @@ const getCurrentGame = () => {
                 numCorrect: 0,
                 numIncorrect: 0,
                 totalQuestions: 10, // for now, this is fixed
-                timePerQuestion: 20, // in seconds; for now, this is fixed
+                timePerQuestion: gamemode === 'progLang' ? 20 : 25,
                 timestamp: new Date().toUTCString()
             };
 
